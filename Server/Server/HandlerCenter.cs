dc6da2cd05384a8b680197c4ec1bc6596b09012b
fuemocheng <fuemocheng@163.com>
2017-11-08 17:55:29 +0800

    2017.11.8

--- Server/Server/HandlerCenter.cs
+++ Server/Server/HandlerCenter.cs
@@ -22,12 +22,12 @@ namespace Server
 
         public override void ClientClose(UserToken token, string error)
         {
-            Console.WriteLine("[ {0} ]断开连接,{1}",token.m_socket.ToString(),error);
+            Console.WriteLine("[ {0} ] 断开连接,{1}",token.m_socket.ToString(),error);
         }
 
         public override void ClientConnect(UserToken token)
         {
-            Console.WriteLine("[ {0} ]连接",token.m_socket.ToString());
+            Console.WriteLine("[ {0} ] 连接",token.m_socket.RemoteEndPoint.ToString());
         }
 
         public override void MessageReceive(UserToken token, object message)
